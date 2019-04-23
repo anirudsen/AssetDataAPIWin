25fbb8e2b3c91b9f4cfc91842537e4be7fc52616
anirud <anirudsen@gmail.com>
2019-04-23 16:40:24 +0530

    1st commit

--- FlaskWeb/views.py
+++ FlaskWeb/views.py
@@ -10,6 +10,15 @@ from FlaskWeb import app
 @app.route('/home')
 def home():
     """Renders the home page."""
+
+    import os
+
+    connectionstring=os.getenv("SQLAZURECONNSTR_mdpappdb")
     
+    import pyodbc
+    conn=pyodbc.connect(connectionstring)
     return "welcome Azure App"
+
+
+    
 
